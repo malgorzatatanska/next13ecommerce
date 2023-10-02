e555aa38037aa0525c7c8a238a15b1a09b2681cf
Malgorzata Tanska <malgosiatanska1@gmail.com>
2023-10-02 22:41:35 +0200

    feat(test-fix-2): single product meta data, active link

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-// import { type Metadata } from "next";
+import { type Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getProductBySlug } from "@/api/products";
 // import { SuggestedProductsList } from "@/ui/organisms/SuggestedProducts";
@@ -13,23 +13,23 @@ import { SingleProduct } from "@/ui/organisms/SingleProduct";
 // 	return productsIdsList.slice(0, 2);
 // };
 
-// export const generateMetadata = async ({
-// 	params,
-// }: {
-// 	params: { slug: string };
-// }): Promise<Metadata> => {
-// 	const product = await getProductBySlug(params.slug);
+export const generateMetadata = async ({
+	params,
+}: {
+	params: { slug: string };
+}): Promise<Metadata> => {
+	const product = await getProductBySlug(params.slug);
 
-// 	return {
-// 		title: `${product?.name}`,
-// 		description: `${product?.description}`,
-// 		openGraph: {
-// 			title: `${product?.name}`,
-// 			description: `${product?.description}`,
-// 			images: [{ url: `${product?.images[0].url}` }],
-// 		},
-// 	};
-// };
+	return {
+		title: `${product?.name}`,
+		description: `${product?.description}`,
+		openGraph: {
+			title: `${product?.name}`,
+			description: `${product?.description}`,
+			images: [{ url: `${product?.images[0].url}` }],
+		},
+	};
+};
 
 export default async function Product({
 	params,
